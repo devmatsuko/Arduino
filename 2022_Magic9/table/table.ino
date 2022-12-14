d75cc53da45c230529b07b166d55b9e56ea9e69d
小松真也 <komatsushinya@192.168.0.4>
2022-12-14 19:55:33 +0900

    Remove unused functions.

--- 2022_Magic9/table/table.ino
+++ 2022_Magic9/table/table.ino
@@ -12,23 +12,22 @@
 #define NUM_LED2 112 // 四角のLEDの数
 #define NUM_LED3 42 // DJ文字のLEDの数
 #define BRIGHTNESS 100 //明るさ
-#define Start 0 //スタート
-#define End 56 //エンド
-#define SQUARE_STAET 42 //スタート
-#define SQUARE_END 112 //エンド
-#define SQUARE_BOTTOM_START1 42 //下辺スタート1
-#define SQUARE_BOTTOM_END1 53 //下辺エンド1
-#define SQUARE_RIGHT_START 54 //右辺スタート
-#define SQUARE_RIGHT_END 63 //右辺エンド
-#define SQUARE_TOP_START 64 //上辺スタート
-#define SQUARE_TOP_END 88 //上辺エンド
-#define SQUARE_LEFT_START 89 //左辺スタート
-#define SQUARE_LEFT_END 98 //左辺エンド
-#define SQUARE_BOTTOM_START2 99 //下辺スタート2
-#define SQUARE_BOTTOM_END2 112 //下辺エンド2
+#define Start 0 // 1本スタート
+#define End 56 // 1本エンド
+#define SQUARE_STAET 42 // 四角スタート
+#define SQUARE_END 112 // 四角エンド
+#define SQUARE_BOTTOM_START1 42 // 下辺スタート1
+#define SQUARE_BOTTOM_END1 53 // 下辺エンド1
+#define SQUARE_RIGHT_START 54 // 右辺スタート
+#define SQUARE_RIGHT_END 63 // 右辺エンド
+#define SQUARE_TOP_START 64 // 上辺スタート
+#define SQUARE_TOP_END 88 // 上辺エンド
+#define SQUARE_LEFT_START 89 // 左辺スタート
+#define SQUARE_LEFT_END 98 // 左辺エンド
+#define SQUARE_BOTTOM_START2 99 // 下辺スタート2
+#define SQUARE_BOTTOM_END2 112 // 下辺エンド2
 
-
-#define TestMode 0 //動作確認用テストモード（On = 1、Off = 0)
+#define TestMode 0 // 動作確認用テストモード（On = 1、Off = 0)
 
 Adafruit_NeoPixel strip[] = {
   Adafruit_NeoPixel(NUM_LED, PIN1, NEO_GRBW + NEO_KHZ800),
@@ -38,9 +37,8 @@ Adafruit_NeoPixel strip[] = {
   Adafruit_NeoPixel(NUM_LED2, PIN5, NEO_GRBW + NEO_KHZ800)
 };
  
-
-//じわじわの値
-  byte neopix_gamma[] = {
+//じわじわの輝度値
+byte neopix_gamma[] = {
   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
   1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
@@ -53,10 +51,11 @@ Adafruit_NeoPixel strip[] = {
   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255
+};
 
 // 色
 const uint32_t white = strip[0].Color(0, 0, 0, 255);
@@ -83,16 +82,15 @@ void setup() {
   strip[2].begin();
   strip[3].begin();
   strip[4].begin();
-  strip[0].show(); // Initialize all pixels to 'off' 
-  strip[1].show(); // Initialize all pixels to 'off' 
-  strip[2].show(); // Initialize all pixels to 'off' 
-  strip[3].show(); // Initialize all pixels to 'off' 
-  strip[4].show(); // Initialize all pixels to 'off' 
+  strip[0].show();
+  strip[1].show();
+  strip[2].show();
+  strip[3].show();
+  strip[4].show();
 }
 
 void loop() {
-  // Some example procedures showing how to display to the pixels:
-  // テストモードがOnの場合は全身1色で光らせるだけ  
+  // テストモードがOnの場合
   if(TestMode == 1){
     fullColorAll(white,2000);
     fullColorAll(red,2000);
@@ -100,7 +98,7 @@ void loop() {
     fullColorAll(green,2000);
     fullColorAll(blue,2000);
     fullColorAll(yellow,2000);
-  }else{    
+  } else {    
     //オープニング(16拍子目にスイッチオン」)
     part_1();
     // 1:08
@@ -117,8 +115,6 @@ void loop() {
    delay(10000000);//ループ防止
 }
 
-
-
 // 4本全部一色で表示する
 void fullColorAll(uint32_t c, uint32_t wait) {
   for(uint16_t i=Start; i<=End; i++) {
@@ -134,7 +130,7 @@ void fullColorAll(uint32_t c, uint32_t wait) {
     delay(wait);
 }
 
-// 4本同時任意に範囲カラー
+// 4本同時に任意の範囲一色で表示する
 void colorWipeRangeAll(uint32_t c, uint32_t wait, uint32_t startLED1, uint32_t endLED1, uint32_t startLED2, uint32_t endLED2, uint32_t startLED3, uint32_t endLED3, uint32_t startLED4, uint32_t endLED4) {
   for(uint16_t i=startLED1; i<=endLED1; i++) {
     strip[0].setPixelColor(i, c);
@@ -155,8 +151,7 @@ void colorWipeRangeAll(uint32_t c, uint32_t wait, uint32_t startLED1, uint32_t e
     delay(wait);
 }
 
-
-// 1本全部一色で表示する
+// 1本の全範囲一色で表示する
 void fullColorOne(uint8_t strip_number, uint32_t c, uint32_t wait) {
   for(uint16_t i=Start; i<=End; i++) {
     strip[strip_number].setPixelColor(i, c);
@@ -165,7 +160,7 @@ void fullColorOne(uint8_t strip_number, uint32_t c, uint32_t wait) {
   delay(wait);
 }
 
-// 1本任意に範囲カラー
+// 1本の任意範囲一色で表示する
 void colorWipeRangeOne(uint8_t strip_number, uint32_t c, uint32_t wait, uint32_t startLED, uint32_t endLED) {
   for(uint16_t i=startLED; i<=endLED; i++) {
     strip[strip_number].setPixelColor(i, c);
@@ -221,7 +216,7 @@ void fullColorSquareOne(uint8_t side ,uint32_t c, uint32_t wait) {
   delay(wait);
 }
 
-//基本リズム1 高さは全て同じ
+// 基本リズム1 高さは全て同じ
 void rhythm1(uint32_t c1, uint32_t c2, uint16_t time, uint8_t speed, uint8_t wait) {
   for(uint16_t i=0; i<=time; i++){
     // 下から上
@@ -256,79 +251,7 @@ void rhythm1(uint32_t c1, uint32_t c2, uint16_t time, uint8_t speed, uint8_t wai
   }
 }
 
-//基本リズム2 高さはバラバラ
-void rhythm2(uint32_t c1, uint32_t c2, uint16_t time, uint8_t wait) {
-  // for(uint16_t i=0; i<=time; i++){
-  //   strip_height1 = random(0, 20);
-  //   strip_height2 = random(0, 20);
-  //   strip_height3 = random(0, 20);
-  //   strip_height4 = random(0, 20);
-  //   // 下から上
-  //   for(uint16_t j=strip[0].numPixels() - 1; j>=2; j=j-2) {
-  //     strip[0].setPixelColor(j+strip_height1, c1);
-  //     strip[1].setPixelColor(j+strip_height2, c1);
-  //     strip[2].setPixelColor(j+strip_height3, c1);
-  //     strip[3].setPixelColor(j+strip_height4, c1);
-  //     strip[0].setPixelColor(j-1+strip_height1, c1);
-  //     strip[1].setPixelColor(j-1+strip_height2, c1);
-  //     strip[2].setPixelColor(j-1+strip_height3, c1);
-  //     strip[3].setPixelColor(j-1+strip_height4, c1);
-  //     strip[0].show();
-  //     strip[1].show();
-  //     strip[2].show();
-  //     strip[3].show();
-  //     delay(wait);
-  //   }
-  //   // 上から下。基本黒
-  //   for(uint16_t k=1; k<strip[0].numPixels(); k=k+2) {
-  //     strip[0].setPixelColor(k+strip_height1, c2);
-  //     strip[1].setPixelColor(k+strip_height2, c2);
-  //     strip[2].setPixelColor(k+strip_height3, c2);
-  //     strip[3].setPixelColor(k+strip_height4, c2);
-  //     strip[0].setPixelColor(k+1+strip_height1, c2);
-  //     strip[1].setPixelColor(k+1+strip_height2, c2);
-  //     strip[2].setPixelColor(k+1+strip_height3, c2);
-  //     strip[3].setPixelColor(k+1+strip_height4, c2);
-  //     strip[0].show();
-  //     strip[1].show();
-  //     strip[2].show();
-  //     strip[3].show();
-  //     delay(wait);
-  //   }
-  //   delay(50);
-  // }
-}
-
-
-// 流れながら点滅(単色)
-void theaterChase(uint32_t c, uint8_t wait,uint8_t cycle ) {
-  for (int j=0; j<cycle; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip[0].numPixels(); i=i+3) {
-        strip[0].setPixelColor(i+q, c);    //turn every third pixel on
-        strip[1].setPixelColor(i+q, c);
-        strip[2].setPixelColor(i+q, c);
-        strip[3].setPixelColor(i+q, c);
-      }
-      strip[0].show();
-      strip[1].show();
-      strip[2].show();
-      strip[3].show();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip[0].numPixels(); i=i+3) {
-        strip[0].setPixelColor(i+q, 0);        //turn every third pixel off
-        strip[1].setPixelColor(i+q, 0);        //turn every third pixel off
-        strip[2].setPixelColor(i+q, 0);        //turn every third pixel off
-        strip[3].setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-
-//じわじわホワイト(はやめ)
+// じわじわ
 void pulseWhite(uint16_t time,uint8_t wait,uint8_t speed,uint32_t c) {
    for(uint16_t k=0; k<=time-1; k++){ 
     for(int j = 0; j < 256 ; j = j+speed){
@@ -382,113 +305,8 @@ void pulseWhite(uint16_t time,uint8_t wait,uint8_t speed,uint32_t c) {
    }
 }
 
-//じわじわブルー(はやめ)
-void pulseBlueIn(uint16_t time,uint8_t wait,uint8_t speed,uint32_t c, uint32_t startLed1, uint32_t endLed1,uint32_t startLed2, uint32_t endLed2,uint32_t startLed3, uint32_t endLed3,uint32_t startLed4, uint32_t endLed4) {
-   for(uint16_t k=0; k<=time-1; k++){ 
-    for(int j = 0; j < 256 ; j = j+speed){
-        for(uint16_t i=startLed1; i<endLed1; i++) {
-            strip[0].setPixelColor(i, strip[0].Color(0,0,neopix_gamma[j] ) );
-        }
-        for(uint16_t i=startLed2; i<endLed2; i++) {
-            strip[1].setPixelColor(i, strip[1].Color(0,0,neopix_gamma[j] ) );
-        }
-        for(uint16_t i=startLed3; i<endLed3; i++) {
-            strip[2].setPixelColor(i, strip[2].Color(0,0,neopix_gamma[j] ) );
-        }
-        for(uint16_t i=startLed4; i<endLed4; i++) {
-            strip[3].setPixelColor(i, strip[3].Color(0,0,neopix_gamma[j]) );
-        }
-          delay(wait);
-          strip[0].show();
-          strip[1].show();
-          strip[2].show();
-          strip[3].show();
-        }
-   }
-}
-
-
-// // Slightly different, this makes the rainbow equally distributed throughout
-// void rainbowCycle(uint8_t wait) {
-//   uint16_t i, j;
-
-//   for(j=0; j<256 * 5; j++) { // 5 cycles of all colors on wheel
-//     for(i=0; i< strip[0].numPixels(); i++) {
-//       strip[0].setPixelColor(i, Wheel(((i * 256 / strip[0].numPixels()) + j) & 255));
-//     }
-//     for(i=0; i< strip[1].numPixels(); i++) {
-//       strip[1].setPixelColor(i, Wheel(((i * 256 / strip[1].numPixels()) + j) & 255));
-//     }
-//     for(i=0; i< strip[2].numPixels(); i++) {
-//       strip[2].setPixelColor(i, Wheel(((i * 256 / strip[2].numPixels()) + j) & 255));
-//     }
-//     for(i=0; i< strip[3].numPixels(); i++) {
-//       strip[3].setPixelColor(i, Wheel(((i * 256 / strip[3].numPixels()) + j) & 255));
-//     }
-//     strip[0].show();
-//     strip[1].show();
-//     strip[2].show();
-//     strip[3].show();
-//     delay(wait);
-//   }
-// }
-
-
-// void rainbow(uint8_t wait) {
-//   uint16_t i, j;
-//   for(j=0; j<256; j=j+4) {
-//     for(i=0; i<strip[0].numPixels(); i++) {
-//       strip[0].setPixelColor(i, Wheel((i+j) & 255));
-//     }
-//     for(i=0; i<strip[1].numPixels(); i++) {
-//       strip[1].setPixelColor(i, Wheel((i+j) & 255));
-//     }
-//     for(i=0; i<strip[2].numPixels(); i++) {
-//       strip[2].setPixelColor(i, Wheel((i+j) & 255));
-//     }
-//     for(i=0; i<strip[3].numPixels(); i++) {
-//       strip[3].setPixelColor(i, Wheel((i+j) & 255));
-//     }
-//     for(i=0; i<strip[4].numPixels(); i++) {
-//       strip[4].setPixelColor(i, Wheel((i+j) & 255));
-//     }
-//     strip[0].show();
-//     strip[1].show();
-//     strip[2].show();
-//     strip[3].show();
-//     strip[4].show();
-//     delay(wait);
-//   }
-// }
-
-// // Input a value 0 to 255 to get a color value.
-// // The colours are a transition r - g - b - back to r.
-// uint32_t Wheel(byte WheelPos) {
-//   WheelPos = 255 - WheelPos;
-//   if(WheelPos < 85) {
-//     return strip[0].Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
-//   }
-//   if(WheelPos < 170) {
-//     WheelPos -= 85;
-//     return strip[0].Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
-//   }
-//   WheelPos -= 170;
-//   return strip[0].Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
-// }
-
-
-// uint8_t red(uint32_t c) {
-//   return (c >> 16);
-// }
-// uint8_t green(uint32_t c) {
-//   return (c >> 8);
-// }
-// uint8_t blue(uint32_t c) {
-//   return (c);
-// }
-
+// レインボー固定
 void rainbow_moment(int wait) {
-
   for(int i=0; i<strip[0].numPixels(); i++) { 
     int pixelHue = (i * 65536L / strip[0].numPixels());
     strip[0].setPixelColor(i, strip[0].gamma32(strip[0].ColorHSV(pixelHue)));
@@ -517,6 +335,7 @@ void rainbow_moment(int wait) {
   delay(wait);
 }
 
+// レインボー可変
 void rainbow2(int wait) {
   for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
     for(int i=0; i<strip[0].numPixels(); i++) { 
@@ -549,7 +368,7 @@ void rainbow2(int wait) {
   
 }
 
-// メインメソッド関連
+// 動作確認用
 void testSquare(uint32_t wait) {
   fullColorSquareAll(strip[0].Color(0, 0, 0, 255),2000);
   fullColorSquareAll(strip[0].Color(0, 155, 255,0),2000);
@@ -810,6 +629,5 @@ void part_3() {
   // 5:20
   // 一瞬ジワジワ
   pulseWhite(1,0,30,white);
-  
 }
 
